4013a630b831c29d51d9216ac451a6b9f6948938
Craig Fowler <craig@csf-dev.com>
2018-05-10 21:57:44 +0100

    See if having more than one null char at the end makes a difference

--- Mono.HttpWebResponse.WebApp/Controllers/MyApiController.cs
+++ Mono.HttpWebResponse.WebApp/Controllers/MyApiController.cs
@@ -24,21 +24,34 @@ namespace Mono.HttpWebResponse.WebApp.Controllers
     {
       var output = new HttpResponseMessage(HttpStatusCode.InternalServerError);
 
-      var outputString = @"This is a particularly long UTF8 string.
-The length may or may not have anything to do with the error of course.
-I only include it because the code where I originally repro'd this had a particularly
-long response.";
+      // To try and be close to the original repro case, I'm generating a long string for the response body
+      var outputString = GetLongResponseText();
 
-      var lotsOfSpam = String.Join(", ", Enumerable.Range(0, 500).Select(x => "spam"));
-      outputString = String.Concat(outputString,  lotsOfSpam);
-
-      // To be closer still to the repo case, I'm adding a trailing null character
-      outputString = String.Concat(outputString, '\0');
+      // To be closer still to the original repro case, I'm adding some trailing null
+      // character (the original repro case has some)
+      outputString = AddSomeTrailingNullCharacters(outputString, 3);
 
       output.Content = new StringContent(outputString);
 
       return output;
     }
 
+    string GetLongResponseText()
+    {
+      var str = @"This is a particularly long UTF8 string.
+The length may or may not have anything to do with the error of course.
+I only include it because the code where I originally repro'd this had a particularly
+long response.";
+
+      var lotsOfSpam = String.Join(", ", Enumerable.Range(0, 500).Select(x => "spam"));
+
+      return String.Concat(str, lotsOfSpam);
+    }
+
+    string AddSomeTrailingNullCharacters(string source, int howMany)
+    {
+      var nullCharacters = new String('\0', howMany);
+      return String.Concat(source, nullCharacters);
+    }
   }
 }
